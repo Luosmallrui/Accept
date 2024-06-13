619a532a87e4a878ba5e3439b07104ab10703e7c
luoxiaorui <luoxiaorui115815@qq.com>
2024-06-13 08:02:16 +0000

    update

--- AERO-GNN-main/.ipynb_checkpoints/AERO-GNN-checkpoint.sh
+++ AERO-GNN-main/.ipynb_checkpoints/AERO-GNN-checkpoint.sh
@@ -1,16 +1,3 @@
-python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 1
-
-python3 ./AERO-GNN/main.py --model aero --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 2
-
-python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 4
-
-python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 8
-
-python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 16
-
-python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 32
-
-python3 ./AERO-GNN/main.py --model aero  --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 64
 
 python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora --iterations 1
 
@@ -25,3 +12,5 @@ python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout
 python3 ./AERO-GNN/main.py --model aero  --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora --iterations 32
 
 python3 ./AERO-GNN/main.py --model aero   --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora --iterations 64
+
+python3 ./AERO-GNN/main.py --model aero  --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset wiki --iterations 64
