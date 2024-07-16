6810ace7cd654a24cfe651b0d338f7bafa7e82ef
luosmallrui <luoxiaorui115815@qq.com>
2024-07-16 03:24:17 +0000

    update

--- AERO-GNN-main/.ipynb_checkpoints/appnp-checkpoint.sh
+++ AERO-GNN-main/.ipynb_checkpoints/appnp-checkpoint.sh
@@ -1,7 +1,7 @@
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  1 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  2 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  4 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  8 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  16 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  32 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
-python3 ./AERO-GNN/main.py --model  appnp  --iterations  64 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset actor
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  1 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  2 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  4 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  8 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  16 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  32 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
+python3 ./AERO-GNN/main.py --model  appnp  --iterations  64 --dr 0.0001 --dr-prop 0.0001 --dropout 0.7 --add-dropout 0 --lambd 1.0  --dataset cora
