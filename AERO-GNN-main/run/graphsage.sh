294cadb56a3d5439217d516302c56008f07ef670
luoxiaorui <luoxiaorui115815@qq.com>
2024-08-01 13:38:19 +0000

    update

--- AERO-GNN-main/run/graphsage.sh
+++ AERO-GNN-main/run/graphsage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define arrays for datasets and iterations
-datasets=("cora" "citeseer" "pubmed" )
+datasets=("wiki" "photo" "computers" )
 iterations=( 1 2 4 8 16 32 64)
 
 # Loop through each dataset and each iteration value
 for dataset in "${datasets[@]}"; do
   for iter in "${iterations[@]}"; do
-    python ../AERO-GNN/main.py --model graphsage --dataset "$dataset" --num-layers "$iter" 
+    python ../AERO-GNN/main.py --model graphsage --dataset "$dataset" --num-layers "$iter"  --device cuda:1
   done
 done
