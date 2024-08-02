442691a06b97d46e276d299629867efffd2db803
Ranran <1969671488@qq.com>
2024-08-02 16:37:50 +0200

    supplement

---
+++ AERO-GNN-main/run/urgent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define arrays for datasets and iterations
+datasets=("cora" "citeseer" "pubmed" "computer" )
+iterations=( 1 2 4 8 16 32 64)
+
+# Loop through each dataset and each iteration value
+for dataset in "${datasets[@]}"; do
+  for iter in "${iterations[@]}"; do
+    python ../AERO-GNN/main.py --model gat --dataset "$dataset" --num-layers "$iter"  --device cuda:1 --dr 1e-2 --dropout 0.5 --lambd-l2 0.0
+  done
+done
+
+python ../AERO-GNN/main.py --model gt --dataset "computer" --num-layers 64  --device cuda:1 --dr 1e-3 --dropout 0.6 --lambd-l2 0.0
+python ../AERO-GNN/main.py --model gat-res --dataset "computer" --num-layers 64  --device cuda:1 --dr 1e-3 --dropout 0.6 --lambd-l2 0.0
